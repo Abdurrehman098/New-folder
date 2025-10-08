b1fd37a012dccb3cefa91eafbfd55eedb413c785
Abdurrehman098 <ktkabdurrehman80@gmail.com>
2025-10-07 21:43:47 -0700

    small programe of loop in which we print the list

---
+++ loops.dart
@@ -0,0 +1,17 @@
+void main() {
+  List<String> friends = [
+    "Muhib Ullah",
+    "Muhammad Haroon",
+    "Farooq Hussain",
+    "Muhammad Uzair",
+    "Muatasim Billah",
+    "Muhammad Awais",
+    "Mussadiq Shah",
+    "Naseer Ahmad",
+    "Abdul Hai",
+  ];
+
+  for (int i = 0; i < friends.length; i++) {
+    print("Your friend's name is: ${friends[i]}");
+  }
+}
